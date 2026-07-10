61ed003f5e3df63faa3e2e4cef11168402ce7eaa
Ian Laidlaw <ian@kaboodles.com>
2026-07-09 20:43:07 -0400

    init commit

---
+++ EmbroideryManagement/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace EmbroideryManagement.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Gary, Indiana!";
+}
---
+++ EmbroideryManagement/Views/MainWindow.axaml.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace EmbroideryManagement.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(Celsius.Text, out var c))
+        {
+            var f = c * (9d / 5d) + 32;
+            Fahrenheit.Text = f.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
+    }
+}
